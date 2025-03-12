2f2321458afaeaf325f5f49d67cff4ca270d4630
adrfstwn <wadewawan375@gmail.com>
2025-03-12 21:35:22 +0700

    update fps

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -41,7 +41,7 @@ function processFrames() {
   const imageData = canvasElement.toDataURL("image/jpeg", 0.7);
   socket.emit("process_frame", { image: imageData });
 
-  setInterval(processFrames, 45);
+  setTiemout(processFrames, 50);
   // setTimeout(processFrames, 35);
   //requestAnimationFrame(processFrames);
 }
